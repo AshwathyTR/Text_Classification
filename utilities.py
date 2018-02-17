982c859a6a51e28401df9fc43697c23df9c1aa38
Apostolis Argatzopoulos <36422627+TheQuietOwl@users.noreply.github.com>
2018-02-17 16:42:33 +0000

    added tftidfvectorizer function

--- utilities.py
+++ utilities.py
@@ -5,7 +5,7 @@ Created on Fri Feb 16 14:37:32 2018
 @author: hp
 """
 
-slang_path = r"C:\Coursework\Advanced ML\slang_dict.txt"
+slang_path = r"slang_dict.txt"
 
 def parse_slang():
     slang_dict={}
@@ -24,3 +24,18 @@ def parse_slang():
         
         
         
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer,TfidfVectorizer
+
+''' insert data here, either train or test and get the baseline vector (or whatever you want to call it) back'''
+def baselineMe(self, data):
+    word_vectorizer = TfidfVectorizer(
+            sublinear_tf=True,
+            strip_accents='unicode',
+            analyzer='word',
+            token_pattern=r'\w{1,}',
+            ngram_range=(1, 1),
+            max_features=15000)
+    word_vectorizer.fit(data)
+    baseline_features = word_vectorizer.transform(data)
+    return baseline_features
