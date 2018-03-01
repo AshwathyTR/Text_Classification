ba7abb321d526f6bd1fc94309026d646c630b251
U-SOTON\apa1n17 <apa1n17@JFB59-R3229-L06.soton.ac.uk>
2018-03-01 17:31:56 +0000

    added 2 features

--- Tester/features.py
+++ Tester/features.py
@@ -4,9 +4,13 @@ Created on Tue Feb 20 16:33:45 2018
 
 @author: Ashwathy 
 """
+import pandas as pd
+import re
 from sklearn.feature_extraction.text import CountVectorizer,TfidfVectorizer
 
 class Extractor:
+    
+    path = r"..\corpora\bad-words.txt"
     
     def get_word_histogram(self, data, vocab_data):
         ''' @params - data :list of comments from which to extract features
@@ -24,3 +28,46 @@ class Extractor:
         histogram = word_vectorizer.transform(data)
         return histogram
     
+    def num_bad_words(self, data):
+        ''' @params - data :list of comments from which to extract features
+            @output - list of number of bad words per sentence
+        '''
+        bad_text=pd.read_csv(self.path,sep="\n", header=None)
+        bad_text.columns=['bad']
+        bad_words=[]
+        for bw in bad_text['bad']:
+            bad_words.append(bw)
+        feature_val=[]
+        for sentence in data:
+            count=0
+            for bw in bad_words:
+                #using regular expressions, each word is seperated by blank or special chars
+                if re.search('(\s|^)'+bw+'(\s|$|\.)',sentence):
+                    print(bw)
+                    count+=1
+            feature_val.append(count)
+        return feature_val
+    
+    def num_Upper_Case(self,data):
+        ''' @params - data :list of comments from which to extract features
+            @output - list of number of upper case characters per sentence
+        '''
+        feature_val=[]
+        for sentence in data:
+            count=0
+            for word in sentence:
+                if word.isupper():
+                    count+=1
+            feature_val.append(count)
+        return feature_val
+
+#  debug testing below
+
+'''
+
+t=Extractor()
+ls="I will kill. you","fuck you friend, you are an idiot","HELLO world"
+print(t.num_bad_words(ls))
+print(t.num_Upper_Case(ls))
+
+'''
